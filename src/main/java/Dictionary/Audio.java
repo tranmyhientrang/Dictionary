58fe9e1aacca20bdd73c64ed63b40f9826926f3d
tranmyhientrang <ttran003945@gmail.com>
2020-10-23 07:08:12 +0700

    Create Audio.java

---
+++ src/main/java/Dictionary/Audio.java
@@ -0,0 +1,36 @@
+package Dictionary;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Audio {
+    private static Audio audio;
+
+    private Audio() {
+    }
+
+    public synchronized static Audio getInstance() {
+        if (audio == null) {
+            audio = new Audio();
+        }
+        return audio;
+    }
+
+    public InputStream getAudio(String text, String languageOutput) throws IOException {
+        URL url = new URL("https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q=" + text.replace(" ", "%20") + "&tl=" + languageOutput);
+        URLConnection urlConn = url.openConnection();
+        urlConn.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+        InputStream audioSrc = urlConn.getInputStream();
+        return new BufferedInputStream(audioSrc);
+    }
+
+    public void play(InputStream sound) throws JavaLayerException {
+        new Player(sound).play();
+    }
+}
